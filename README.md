546b8b254556f4b0d3066eac60e3c4114c43b190
Tyler Henkel <tylerhenkel@hotmail.com>
2013-12-24 12:51:04 -0500

    updated initial commit

--- README.md
+++ README.md
@@ -1,29 +1,9 @@
 ## Showcase features of your website
 
-## Pure AngularJS, No Jquery
-
 ## Tested against all major browsers
 
 ## Easy to use
 
+You'll need jquery and angular-cookies for this to work.
+
 ## Try it out
-
-
-<tour placement="{{ tt_placement }}"
- 	animation="tt_animation"
- 	scroll-speed="500">
-
-  <tour-tip 
-  	title="{{ tt_title }}"
-  	content="{{ tt_content }}"
-  	is-open="true"
-  	>
-  </tour-tip>    	
-
-tour directive should
-  scroll from one step to the next
-  change state of step to active
-  when step is active it should create a tour-tip
-  alternatively tour-tip could be the equivalent of step
-  and keep track of the current position
-
--- src/tour/tour.spec.js
+++ src/tour/tour.spec.js
@@ -20,29 +20,57 @@ describe('Directive: tour', function () {
     beforeEach(inject(function (OrderedList) {
       steps = OrderedList;
       // add unordered items
+    }));
+
+    function addTypicalSteps() {
       steps.set(1, '1');
       steps.set(3, '3');
       steps.set(2, '2');
       steps.set(4, '4');
-    }));
+    }
+
+    it('should insert a value into steps', function () {
+      steps.set(2, '2');
+      steps.set(3, '3');
+      steps.set(4, '4');
+
+      expect(steps.getCount()).toBe(3);
+      steps.set(1, '1');
+      expect(steps.getCount()).toBe(4);
+
+      expect(steps.get(1)).toBe('1');
+      expect(steps.get(2)).toBe('2');
+      expect(steps.get(3)).toBe('3');
+      expect(steps.get(4)).toBe('4');
+    });
+
+    it('should should return the first value', function () {
+      addTypicalSteps();
+      expect(steps.first()).toBe('1');
+    });
 
     it('should find index of \'4\'', function () {
+      addTypicalSteps();
       expect(steps.indexOf('4'));
     });
     it('should have a length of 4 items', function () {
+      addTypicalSteps();
       expect(steps.getCount()).toBe(4);
     });
 
     it('should not add undefined indexes', function () {
+      addTypicalSteps();
       steps.set(undefined, '5');
       expect(steps.getCount()).toBe(4);
     });
 
     it('should get the 3rd item', function() {
+      addTypicalSteps();
       expect(steps.get(3)).toBe('3');
     });
 
     it('should push a value into steps', function () {
+      addTypicalSteps();
       expect(steps.getCount()).toBe(4);
       steps.push('5');
       expect(steps.getCount()).toBe(5);
@@ -50,10 +78,6 @@ describe('Directive: tour', function () {
     });
 
     it('should push a value into empty list', inject(function (OrderedList) {
-      steps.remove(1);
-      steps.remove(2);
-      steps.remove(3);
-      steps.remove(4);
       expect(steps.getCount()).toBe(0);
 
       steps.push('1');
@@ -62,8 +86,9 @@ describe('Directive: tour', function () {
     }));
 
     it('should order numbers properly', function() {
+      addTypicalSteps();
       var list = [];
-      steps.forEach(function(key, value) {
+      steps.forEach(function(value, key) {
         list.push(key);
       });
       expect(list[0]).toBe(1);
@@ -80,10 +105,11 @@ describe('Directive: tour', function () {
       this.addMatchers({
         toHaveOpenTourtips: function(noOfOpened) {
           var tourtipElements = this.actual.find('div.tour-tip');
-          noOfOpened = noOfOpened || 1;
+          noOfOpened = noOfOpened;
 
           this.message = function() {
-            return "Expected '" + angular.mock.dump(tourtipElements) + "' to have '" + tourtipElements.length + "' opened tour tips.";
+            return 'Expected \'' + angular.mock.dump(tourtipElements) + '\' to have \'' + noOfOpened +
+              '\' opened tour tips. Instead had \'' + tourtipElements.length + '\'.';
           };
 
           return tourtipElements.length === noOfOpened;
@@ -93,12 +119,12 @@ describe('Directive: tour', function () {
       scope = $rootScope.$new();
 
       tour = angular.element('<tour></tour>');
-      tip1 = angular.element('<span tour-tip="feature 1!" step="0" next="Next" placement="top" class="btn">' +
+      tip1 = angular.element('<span tour-tip="feature 1!" step="0" next-label="Next" placement="top" class="btn">' +
         'Important website feature' +
         '<a href="#" tour-next></a>' +
         '</span>');
 
-      tip2 = angular.element('<span tour-tip="feature 2!" step="1" next="Next" placement="top" class="btn">' +
+      tip2 = angular.element('<span tour-tip="feature 2!" step="1" next-label="Next" placement="top" class="btn">' +
         'Another website feature' +
         '</span>');
 
@@ -107,11 +133,28 @@ describe('Directive: tour', function () {
 
       elm = $compile(tour)(scope);
       scope.$apply();
+      $timeout.flush();
 
-      tourScope = elm.isolateScope();
+      tourScope = elm.scope();
     });
     afterEach(function() {
       scope.$destroy();
+    });
+
+    it('should be able to close tour', function () {
+      expect(elm).toHaveOpenTourtips(1);
+      tourScope.closeTour();
+      scope.$apply();
+      expect(elm).toHaveOpenTourtips(0);
+    });
+
+    it('should be able to open tour', function () {
+      tourScope.closeTour();
+      scope.$apply();
+      expect(elm).toHaveOpenTourtips(0);
+      tourScope.openTour();
+      scope.$apply();
+      expect(elm).toHaveOpenTourtips(1);
     });
 
     describe('tour-tip', function() {
@@ -119,29 +162,49 @@ describe('Directive: tour', function () {
         expect(elm.html()).toContain('Important website feature');
       });
 
-      it('should append feature 1 popup to element and open it', function () {
+      it('should append tip1 popup to element and open it', function () {
         expect(elm).toHaveOpenTourtips(1);
         expect(tip1.next().html()).toContain('feature 1');
-        expect(tip1.scope().tt_open).toBe(true);
+        expect(tip1.isolateScope().tt_open).toBe(true);
       });
 
-      it('should open feature 2 popup to element', function () {
-        tourScope.next();
+      it('should open tip2 popup and close tip1 on next', function () {
+        var elmNext = elm.find('.tour-next-tip').eq(0);
+        elmNext.click();
+
         expect(elm).toHaveOpenTourtips(1);
-        expect(tip2.scope().tt_open).toBe(true);
+        expect(tip1.isolateScope().tt_open).toBe(false);
+        expect(tip2.isolateScope().tt_open).toBe(true);
       });
 
+      it('should close tips when you click close', function () {
+        var elmNext = elm.find('.tour-close-tip').eq(0);
+        elmNext.click();
+
+        expect(tip1.isolateScope().tt_open).toBe(false);
+        expect(tip2.isolateScope().tt_open).toBe(false);
+      });
     });
   });
 
   describe('tour controller', function() {
-    var scope, ctrl;
+    var scope, ctrl, mockCookieService;
     //create an array of steps and add to the scope
-    var steps = [{'content': 1},{'content': 2}];
+    var steps = [{'content': 1},{'content': 2},{'content': 3}];
 
     beforeEach(function() {
+      mockCookieService = {
+        _cookies: {},
+        get: function(key) {
+          return this._cookies[key];
+        },
+        put: function(key, value) {
+          this._cookies[key] = value;
+        }
+      };
+
       scope = $rootScope.$new();
-      ctrl = $controller('TourController', {$scope: scope});
+      ctrl = $controller('TourController', {$scope: scope,  $cookieStore: mockCookieService});
       for(var i = 0;i < steps.length;i++){
         ctrl.addStep(steps[i]);
       }
@@ -152,28 +215,109 @@ describe('Directive: tour', function () {
       scope.$destroy();
     });
 
+    it('should not open tour if it was closed ', function () {
+      ctrl.next();
+      ctrl.endTour();
+
+      ctrl = $controller('TourController', {$scope: scope});
+      for(var i = 0;i < steps.length;i++){
+        ctrl.addStep(steps[i]);
+      }
+      expect(ctrl.getCurrentStep()).toBeFalsy();
+    });
+
+    it('should load step when you reopen tour ', function () {
+      ctrl.startTour();
+      ctrl.next();
+      ctrl.endTour(true);
+      ctrl.select(steps[0]);
+
+      ctrl = $controller('TourController', {$scope: scope, $cookieStore: mockCookieService});
+      for(var i = 0;i < steps.length;i++){
+        ctrl.addStep(steps[i]);
+      }
+      scope.$apply();
+      ctrl.startTour();
+
+      expect(ctrl.getCurrentStep()).toEqual(steps[1]);
+    });
+
+    it('should set tour to completed when you reach end', inject(function (tourConfig) {
+      expect(mockCookieService.get(tourConfig.cookieName + '_completed')).toBe(undefined);
+      ctrl.startTour();
+      ctrl.next();
+      ctrl.next();
+      ctrl.next();
+      expect(mockCookieService.get(tourConfig.cookieName + '_completed')).toBe(true);
+    }));
+
+    it('should select step at index', function () {
+      ctrl.select(steps[0]);
+      expect(ctrl.getCurrentStep()).toEqual(steps[0]);
+      ctrl.selectAtIndex(1);
+      expect(ctrl.getCurrentStep()).toEqual(steps[1]);
+    });
+
+    it('should set first step to active = true and the rest to false', function() {
+      ctrl.startTour();
+
+      ctrl.steps.forEach(function(step, i) {
+        if (i !== 0) {
+          expect(step.tt_open).not.toBe(true);
+        } else {
+          expect(step.tt_open).toBe(true);
+        }
+      });
+    });
+
     it('should have added tour-tips to steps array', function () {
-      expect(ctrl.steps.getCount()).toBe(2);
+      expect(ctrl.steps.getCount()).toBe(3);
     });
 
     it('should add tour-tip to end of list if it doesnt specify step', function () {
-      expect(ctrl.steps.get(2)).toBe(undefined);
-      expect(ctrl.steps.getCount()).toBe(2);
+      expect(ctrl.steps.get(3)).toBe(undefined);
+      expect(ctrl.steps.getCount()).toBe(3);
 
       var tourStep = {content:'mockStep'};
       ctrl.addStep(tourStep);
 
-      expect(ctrl.steps.getCount()).toBe(3);
-      expect(ctrl.steps.get(2).content).toBe('mockStep');
+      expect(ctrl.steps.getCount()).toBe(4);
+      expect(ctrl.steps.get(3).content).toBe('mockStep');
     });
 
     it('should replace tour-tip at specified step', function () {
       expect(ctrl.steps.get(0).content).toBe(1);
 
-      var tourStep = {content: 3, position:0};
+      var tourStep = {content: 3, index:0};
       ctrl.addStep(tourStep);
 
       expect(ctrl.steps.get(0).content).toBe(3);
+    });
+  });
+
+  describe('scroll service', function() {
+    var target, scope, scrollTo;
+
+    beforeEach(inject(function (_scrollTo_) {
+      scope = $rootScope.$new();
+      scrollTo = _scrollTo_;
+
+      target = angular.element("<div id=\"target\" style=\"position:absolute; top:200px;\"></div>");
+      $('body').height(window.innerHeight*2).append(target);
+      window.scrollTo(0, 0);
+    }));
+
+    it('should scroll to position', function () {
+      expect($(window).scrollTop()).toEqual(0);
+
+      scrollTo(target);
+      waitsFor(function() {
+        return $(window).scrollTop() === 100;
+      }, 'Current position to be 100px');
+
+      runs(function() {
+        expect($(window).scrollTop()).toEqual(100);
+      });
     });
   });
 });

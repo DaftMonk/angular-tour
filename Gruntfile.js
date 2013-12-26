96c5c063b463e93ae43c0c1f62233e45dc7c5e5d
Tyler Henkel <tylerhenkel@hotmail.com>
2013-12-26 17:32:26 -0500

    chore(gruntfile): bump task now commits

--- Gruntfile.js
+++ Gruntfile.js
@@ -355,13 +355,13 @@ module.exports = function (grunt) {
           'package.json',
           'bower.json'
         ],
-        commit: false,
+        commit: true,
         commitMessage: 'chore(release): v%VERSION%',
         commitFiles: [
           'package.json',
           'bower.json'
         ],
-        createTag: false,
+        createTag: true,
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
         push: false,

c34fdefa14d89d1669a73c2d3fb5df1ff41cf2dc
Piotrek Dąbrowski <floubadour@gmail.com>
2014-11-05 16:48:13 +0000

    chore(docs): updated docs and demo

--- dist/angular-tour.js
+++ dist/angular-tour.js
@@ -1,6 +1,6 @@
 /**
  * An AngularJS directive for showcasing features of your website
- * @version v0.1.1 - 2014-10-31
+ * @version v0.1.1 - 2014-11-05
  * @link https://github.com/DaftMonk/angular-tour
  * @author Tyler Henkel
  * @license MIT License, http://www.opensource.org/licenses/MIT
